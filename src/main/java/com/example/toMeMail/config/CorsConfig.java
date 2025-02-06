d16684faf579ef2b6f7cfda0fe52d118b2fce28b
Marco Vomiero <colocomarco1@gmail.com>
2025-02-06 22:58:50 +0100

    add config for CORS and update security

---
+++ src/main/java/com/example/toMeMail/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.example.toMeMail.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins("https://icy-river-06c72c503.4.azurestaticapps.net")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("Authorization", "Content-Type")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
+
