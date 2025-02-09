2bfc1d2db3c43978c2f9da725c30267256db760f
Marco Vomiero <colocomarco1@gmail.com>
2025-02-09 12:15:00 +0100

    remove id from database since it is AUTO_INCREMENT

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,10 @@
 -- Insert Users with pre-hashed passwords
-INSERT INTO users (id, user_name, password, role, date_of_birth) VALUES
-(1, 'alice', '$2a$10$P6BqKbu29rxQkzB2cPX.z.oANqBZ/TpQxA63HKUeYqGuxKKzBHiXC', 'USER', '1995-08-15');
+INSERT INTO users (user_name, password, role, date_of_birth) VALUES
+('alice', '$2a$10$P6BqKbu29rxQkzB2cPX.z.oANqBZ/TpQxA63HKUeYqGuxKKzBHiXC', 'USER', '1995-08-15');
 -- password = 'testPassword'
 
 -- Insert Messages linked to users
-INSERT INTO messages (id, content, due_date, creation_date, user_id) VALUES
-(1, 'Hello Future Alice!', '2025-01-01 10:00:00', '2015-01-01 10:00:00', 1),
-(2, 'Remember to invest!', '2030-06-15 12:00:00', '2015-01-01 10:00:00', 1);
+INSERT INTO messages (content, due_date, creation_date, user_id) VALUES
+('Hello Future Alice!', '2025-01-01 10:00:00', '2015-01-01 10:00:00', 1),
+('Remember to invest!', '2030-06-15 12:00:00', '2015-01-01 10:00:00', 1);
+
